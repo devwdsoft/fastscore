761431b3e11db0d569e741829aeb8b387ff94046
Cao Van Cuong <vhagar@MacBook-Pro-cua-Cao.local>
2025-12-09 23:25:21 +0700

    update json config

--- src/main/kotlin/crawler/ConfigCrawler.kt
+++ src/main/kotlin/crawler/ConfigCrawler.kt
@@ -1,6 +1,7 @@
 package crawler
 
 import extension.getEnv
+import kotlinx.serialization.json.Json
 import model.Config
 import model.LeagueTableConfig
 import util.MyUtil
@@ -10,13 +11,21 @@ object TableConfigCrawler {
 
     internal val tableConfigPath = "fastscore/config/table_config.json"
 
+    // JSON serializer that excludes null values
+    private val jsonWithoutNulls = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+        encodeDefaults = false
+        explicitNulls = false
+    }
+
     fun crawlConfig() {
         val newConfig = MyUtil.fetchFromUrl(
             getEnv("CONFIG_URL").orEmpty(),
             LeagueTableConfig.serializer()
         )?.toConfig()
         if (newConfig != null) {
-            File(tableConfigPath).writeText(MyUtil.json.encodeToString(Config.serializer(), newConfig))
+            File(tableConfigPath).writeText(jsonWithoutNulls.encodeToString(Config.serializer(), newConfig))
         }
     }
 }
